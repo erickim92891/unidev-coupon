c85f92488c6364aaf65fd99b37bd13d28a4aeaf1
erickim928 <erickim928@gmail.com>
2015-09-30 16:54:52 -0400

    Removed the sidebar function from the navbar directive.

--- angular/app/widgets/navbar/default.navbar.directive.js
+++ angular/app/widgets/navbar/default.navbar.directive.js
@@ -9,12 +9,6 @@
 		return {
 			restrict: 'E',
 			templateUrl: 'app/widgets/navbar/default.navbar.template.html',
-			link: Link
 		};
-		
-		function Link ($scope, $element, $attr) {
-			$ ('#widget-default-navbar-sidebar')
-				.sidebar ('attach events', '#widget-default-navbar .toc.item');
-		}
 	}
 }) ();
