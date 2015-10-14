bced2eafa26814360af7ad94db7badbc33de44f5
erickim928 <erickim928@gmail.com>
2015-10-13 21:41:45 -0400

    Added in a directive for a time filter

---
+++ angular/app/widgets/time/time-filter.directive.js
@@ -0,0 +1,63 @@
+/**
+ * Time filter directive
+ * @namespace Directive
+ *
+ * @memberof Application.Widget.Time
+ */
+(function () {
+	'use strict';
+	
+	angular
+		.module ('widgets.time')
+		.directive ('widgetTimeFilter', TimeFilter);
+	
+	// Dependencies
+	TimeFilter.$inject = [
+		'_',
+		'$time',
+		'$timeConstant'
+	];
+	
+	/**
+	 * @function TimeFilter
+	 * @return {Object} - Directive parameters
+	 * 
+	 * @memberof Application.Widget.Time.Directive
+	 */
+	function TimeFilter (_, $time, $timeConstant)  {
+		// Default directive values used in the element attributes
+		var defaultValues = {
+			align: 'left',
+			icon: 'calendar'
+		};
+		
+		return {
+			restrict: 'E',
+			templateUrl: 'app/widgets/time/time-filter.template.html',
+			scope: {
+				align: '@?',
+				icon: '@?',
+				color: '@?',
+				iconColor: '@?'
+			},
+			link: Link
+		};
+		
+		/**
+		 * @function Link
+		 * @desc Assign the default values for the time filter directive
+		 */
+		function Link ($scope, $element, $attrs) {
+			$scope.time = $time;
+			$scope.timeConstant = $timeConstant;
+			
+			if (_.isUndefined ($scope.align)) {
+				$scope.align = defaultValues.align;
+			}
+			
+			if (_.isUndefined ($scope.icon)) {
+				$scope.icon = defaultValues.icon;
+			}
+		}
+	}
+}) ();
---
+++ angular/app/widgets/time/widgets.time.module.js
@@ -0,0 +1,14 @@
+/**
+ * Time module
+ * @namespace Time
+ *
+ * @memberof Application.Widget
+ */
+(function () {
+	'use strict';
+	
+	angular
+		.module ('widgets.time', [
+			'core.time'
+		]);
+}) ();
