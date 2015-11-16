cf31a9b88248584d0aace0547b93f65012e801b9
erickim928 <erickim928@gmail.com>
2015-11-16 07:59:32 +0000

    Made changes to the dashboard controller to add in 3 new graphs; Coupons Redeemed, Top Coupons, and Hot Areas

--- angular/app/dashboard/dashboard.controller.js
+++ angular/app/dashboard/dashboard.controller.js
@@ -6,26 +6,98 @@
 		.controller ('dashboardCtrl', Dashboard);
 	
 	Dashboard.$inject = [
-		'$time'
+		'$time',
+		'$timeConstant',
+		'RedeemedCoupons',
+		'$scope',
+		'_',
+		'$unidevFirebase',
+		'Graph',
+		'$geocode'
 	];
 	
-	function Dashboard ($time) {
+	function Dashboard ($time, $timeConstant, RedeemedCoupons, $scope, 
+			_, $unidevFirebase, Graph, $geocode) {
 		var vm = this;
+
+		vm.time = $time;
+
+		vm.couponsRedeemedGraph = new Graph ();
+		vm.topCouponGraph = new Graph ();
+		vm.hotAreasGraph = new Graph ();
 		
-		vm.time = 'Today';
-		vm.couponsRedeemed = 40503;
-		vm.topCoupon = 'BOGO';
-		vm.hotAreas = 'Anaheim, 92801';
-	
-		vm.series = ['Series A', 'Series B'];
-		vm.labels = $time.generateWeekRange ();
-		vm.data = [
-			[65, 59, 80, 81, 56, 55, 65],
-			[28, 48, 40, 19, 86, 27, 72]
-		];
+		vm.couponsRedeemedGraph.setName ('Coupons Redeemed');
+		vm.topCouponGraph.setName ('Coupon Count');
+		vm.hotAreasGraph.setName ('Hot Areas');
+
+		vm.coupons = RedeemedCoupons ($timeConstant.TODAY);
 		
-		vm.data1 = [65, 59, 80, 81, 56, 55, 40];
+		$scope.$watch (function () {
+			return $time.getCurrentTime ();
+		}, function (newTime) {
+			vm.coupons.$destroy ();
+			vm.coupons = RedeemedCoupons (newTime);
+		});
 		
+		$scope.$watch (function () {
+			return vm.coupons;
+		}, function () {
+			vm.coupons.$loaded (function (coupons) {
+				// Should ONLY be executed after vm.coupons is completely loaded
+				init ();
+				
+				vm.coupons.$watch (function () {
+					init ();
+				});
+			});
+		});
 		
+		function init () {
+			initDashboardAlertCards ();
+			initCouponsRedeemedGraph ();
+			initTopCouponGraph ();
+			initHotAreasGraph ();
+		}
+		
+		function initDashboardAlertCards () {
+			vm.numRedeemed = vm.coupons.count ();
+			vm.topCoupon = vm.coupons.topCoupon ();
+			vm.hotAreas = vm.coupons.hotArea ();
+		}
+		
+		function initCouponsRedeemedGraph () {
+			vm.couponsRedeemedGraph.setLabels ($time.generateCurrentTimeRange ());
+			vm.couponsRedeemedGraph.setData (function () {
+				var data = [];
+				var groupedData = vm.coupons.groupByCurrentTime ();
+				
+				_.each (groupedData, function (group) {
+					data.push (group.length);
+				});
+
+				return [data];
+			});
+		}
+		
+		function initTopCouponGraph () {
+			var redeemedCoupons = vm.coupons.redeemedCoupons();
+			vm.topCouponGraph.setLabels (_.keys (redeemedCoupons));
+			vm.topCouponGraph.setData (_.values (redeemedCoupons));
+		}
+		
+		function initHotAreasGraph () {
+			var redeemedCoupons = vm.coupons.hotAreas (5);
+			var labels = [];
+			var data = [];
+			
+			_.each (redeemedCoupons, function (pair) {
+				labels.push (pair[0]);
+				data.push (pair[1]);
+			});
+			
+			vm.hotAreasGraph.setLabels (labels);
+			vm.hotAreasGraph.setData (data);
+		}
 	}
+	
 }) ();
